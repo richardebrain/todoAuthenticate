db47fe35f52e27422f3413d58ed22b660371ff85
richardebrain <richardebrain20@gmail.com>
2022-06-26 22:09:01 +0100

    fixing git not updating auth0

--- pages/api/utils/auth0.js
+++ pages/api/utils/auth0.js
@@ -6,7 +6,7 @@ export default initAuth0({
   clientID: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
   baseURL: "http://localhost:3000/",
-
+  
 
 });
 
