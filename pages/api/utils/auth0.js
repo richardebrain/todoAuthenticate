9a05c1e0af71950be87bce050284f8ff2da090f5
richardebrain <richardebrain20@gmail.com>
2022-06-27 16:15:21 +0100

    setting env

--- pages/api/utils/auth0.js
+++ pages/api/utils/auth0.js
@@ -1,13 +1,12 @@
 import { initAuth0 } from "@auth0/nextjs-auth0";
 
 export default initAuth0({
-  secret: process.env.AUTH0_SECRET,
+  secret: process.env.NEXT_PUBLIC_AUTH0_SECRET,
   issuerBaseURL:'https://richardebrain.us.auth0.com',
   clientID: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID,
-  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  clientSecret: process.env.NEXT_PUBLIC_AUTH0_CLIENT_SECRET,
   baseURL: "http://localhost:3000/",
   
-
 });
 
 
