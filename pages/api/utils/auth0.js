c4de2d795e076242fa647c0ab59dc5aa25d2a85c
richardebrain <richardebrain20@gmail.com>
2022-06-28 12:12:23 +0100

    base url config

--- pages/api/utils/auth0.js
+++ pages/api/utils/auth0.js
@@ -1,11 +1,11 @@
 import { initAuth0 } from "@auth0/nextjs-auth0";
-
+const baseUrlSettings = process.env.NODE_ENV === "production" ? "https://todo-authenticate.vercel.app/" : "http://localhost:3000";
 export default initAuth0({
   secret: process.env.NEXT_PUBLIC_AUTH0_SECRET,
   issuerBaseURL:'https://richardebrain.us.auth0.com',
   clientID: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID,
   clientSecret: process.env.NEXT_PUBLIC_AUTH0_CLIENT_SECRET,
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  baseURL: baseUrlSettings,
   
 });
 
