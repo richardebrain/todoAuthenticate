963a399b52a88be0e6a61fd6c9adc9348ff3d734
richardebrain <richardebrain20@gmail.com>
2022-06-28 13:14:44 +0100

    corrected mistakes of base url

--- pages/api/utils/auth0.js
+++ pages/api/utils/auth0.js
@@ -1,5 +1,5 @@
 import { initAuth0 } from "@auth0/nextjs-auth0";
-const baseUrlSettings = process.env.NODE_ENV === "production" ? "todo-authenticate-richardebrain.vercel.app" : "http://localhost:3000";
+const baseUrlSettings = process.env.NODE_ENV === "production" ? "https://todo-authenticate.vercel.app/" : "http://localhost:3000";
 export default initAuth0({
   secret: process.env.NEXT_PUBLIC_AUTH0_SECRET,
   issuerBaseURL:'https://richardebrain.us.auth0.com',
