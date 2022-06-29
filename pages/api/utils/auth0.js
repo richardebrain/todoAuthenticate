77640616faf2068ca790ac645972c1518d97db3d
richardebrain <richardebrain20@gmail.com>
2022-06-29 23:46:19 +0100

    fixed auht0 scope error

--- pages/api/utils/auth0.js
+++ pages/api/utils/auth0.js
@@ -6,14 +6,18 @@ export default initAuth0({
   clientID: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID,
   clientSecret: process.env.NEXT_PUBLIC_AUTH0_CLIENT_SECRET,
   baseURL: baseUrlSettings,
+ 
   
   routes: {
+    
     callback: process.env.NEXT_PUBLIC_REDIRECT_URI,
     postLogoutRedirect:
       process.env.NEXT_PUBLIC_POST_LOGOUT_REDIRECT_URI ||
       'http://localhost:3000',
-  }
- 
+  },
+authorizationParams:{
+  scope:process.env.NEXT_PUBLIC_AUTH0_SCOPE
+}
   
 });
 
