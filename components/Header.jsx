f106e5fb7061868b7bda44e7e75ae319ac991ac1
richardebrain <richardebrain20@gmail.com>
2022-06-30 00:12:54 +0100

    changed some few auth0 config

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,13 +4,13 @@ const Header = () => {
     <header className="flex justify-between w-11/12 mx-auto mt-6 items-center py-4">
       <nav className="2xl font-bold text-gray-800">My Todos</nav>
       <div className="flex ">
-        <Link href="/api/auth/logout">
+        <Link href="/api/logout">
           <a className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4">
             Logout
           </a>
         </Link>
 
-        <Link href="/api/auth/login">
+        <Link href="/api/login">
           <a className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4">
             Login
           </a>
